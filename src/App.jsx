dd2d65283979a58bb87f9ebefc3ae708e01faaed
Ryan Hayes <ryanhayes923@gmail.com>
2023-07-21 18:06:37 -0400

    Use React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,13 @@
-import { Box, Grid, Typography } from "@mui/material/";
-import { LinkedIn, GitHub } from "@mui/icons-material/";
-import Fade from "react-reveal/Fade";
-import "./assets/App.css";
-
-const gitHubUrl = "https://www.github.com/rhayes923";
-const linkedInUrl = "https://www.linkedin.com/in/rhayes923/";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Home.jsx";
 
 export default function App() {
   return (
-    <Box className="Box">
-      <Grid
-        container
-        spacing={1}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        sx={{ minHeight: "80vh" }}
-      >
-        <Fade bottom cascade>
-          <Typography sx={{ fontSize: "5rem", fontFamily: "Caveat" }}>
-            Ryan Hayes
-          </Typography>
-          <Typography sx={{ fontSize: "3rem", fontFamily: "Caveat" }}>
-            Associate Full Stack Engineer
-          </Typography>
-          <Typography sx={{ fontSize: "3rem", fontFamily: "Caveat" }}>
-            Lockheed Martin
-          </Typography>
-          <Grid container spacing={5} sx={{ marginTop: "5px" }}>
-            <Grid item>
-              <LinkedIn
-                sx={{ fontSize: 50, color: "#0077B5", cursor: "pointer" }}
-                onClick={() => window.open(linkedInUrl)}
-              />
-            </Grid>
-            <Grid item>
-              <GitHub
-                sx={{ fontSize: 50, cursor: "pointer" }}
-                onClick={() => window.open(gitHubUrl)}
-              />
-            </Grid>
-          </Grid>
-        </Fade>
-      </Grid>
-    </Box>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,48 @@
+import { Box, Grid, Typography } from "@mui/material/";
+import { LinkedIn, GitHub } from "@mui/icons-material/";
+import Fade from "react-reveal/Fade";
+import "./assets/Home.css";
+
+const gitHubUrl = "https://www.github.com/rhayes923";
+const linkedInUrl = "https://www.linkedin.com/in/rhayes923/";
+
+export default function Home() {
+  return (
+    <Box className="Box">
+      <Grid
+        container
+        spacing={1}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "80vh" }}
+      >
+        <Fade bottom cascade>
+          <Typography sx={{ fontSize: "5rem", fontFamily: "Caveat" }}>
+            Ryan Hayes
+          </Typography>
+          <Typography sx={{ fontSize: "3rem", fontFamily: "Caveat" }}>
+            Associate Full Stack Engineer
+          </Typography>
+          <Typography sx={{ fontSize: "3rem", fontFamily: "Caveat" }}>
+            Lockheed Martin
+          </Typography>
+          <Grid container spacing={5} sx={{ marginTop: "5px" }}>
+            <Grid item>
+              <LinkedIn
+                sx={{ fontSize: 50, color: "#0077B5", cursor: "pointer" }}
+                onClick={() => window.open(linkedInUrl)}
+              />
+            </Grid>
+            <Grid item>
+              <GitHub
+                sx={{ fontSize: 50, cursor: "pointer" }}
+                onClick={() => window.open(gitHubUrl)}
+              />
+            </Grid>
+          </Grid>
+        </Fade>
+      </Grid>
+    </Box>
+  );
+}
