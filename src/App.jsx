548361ed2b49a7b72dab525609329c475d79e2b9
Ryan Hayes <ryanhayes923@gmail.com>
2023-07-21 23:53:55 -0400

    Try BrowserRouter again

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Home.jsx";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
