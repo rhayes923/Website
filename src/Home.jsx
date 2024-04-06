5a788b8ff5bb45bb11355b364fd67c0539608028
Ryan Hayes <ryanhayes923@gmail.com>
2024-04-06 01:19:48 -0400

    Moving text

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Box, Grid, Typography } from "@mui/material/";
 import { LinkedIn, GitHub } from "@mui/icons-material/";
 import { Fade } from "react-awesome-reveal";
@@ -7,9 +8,33 @@ const gitHubUrl = "https://www.github.com/rhayes923";
 const linkedInUrl = "https://www.linkedin.com/in/rhayes923/";
 
 export default function Home() {
+
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateMousePosition = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
+
+  useEffect(() => {
+    const container = document.querySelector('.text');
+    if (container) {
+      const translateX = (mousePosition.x - window.innerWidth / 2) / 80;
+      const translateY = (mousePosition.y - window.innerHeight / 2) / 80;
+      container.style.setProperty('--translateX', `${translateX}px`);
+      container.style.setProperty('--translateY', `${translateY}px`);
+    }
+  }, [mousePosition]);
+
   return (
     <Box className="Box">
-      <Grid container spacing={1} direction="column" alignItems="center">
+      <Grid container spacing={1} direction="column" alignItems="center" className="text">
         <Fade cascade direction="up">
           <Grid item pb={2}>
             <Typography variant="h1">
